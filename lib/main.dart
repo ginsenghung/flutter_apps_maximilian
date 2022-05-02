4829364214f674360bbf4c5cec7035930863efa3
Ginseng Hung Prin <gprin94@gmail.com>
2022-05-02 11:59:32 -0400

    building an app from scratch

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,31 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:fluent_ui/fluent_ui.dart' as fluent_ui;
+
+bool get isDesktop {
+  if (kIsWeb) return false;
+  return [
+    TargetPlatform.windows,
+    TargetPlatform.linux,
+    TargetPlatform.macOS,
+  ].contains(defaultTargetPlatform);
+}
+
 void main() {
   
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Platform.isWindows
+        ? fluent_ui.FluentApp(
+            home: Text('Hello!'),
+          )
+        : MaterialApp(
+            home: Text('Hello!'),
+          );
+  }
 }
