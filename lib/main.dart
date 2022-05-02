f5b23154437495ea6360d499810434b71bc0821d
Ginseng Hung Prin <gprin94@gmail.com>
2022-05-02 14:39:20 -0400

    buiding a widget tree

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 import 'package:fluent_ui/fluent_ui.dart' as fluent_ui;
+import 'package:flutter_acrylic/flutter_acrylic.dart' as flutter_acrylic;
+import 'package:window_manager/window_manager.dart';
 
 bool get isDesktop {
   if (kIsWeb) return false;
@@ -13,21 +15,41 @@ bool get isDesktop {
   ].contains(defaultTargetPlatform);
 }
 
-// void main() {
-//   runApp(MyApp());
-// }
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-void main() => runApp(MyApp());
+  if (isDesktop) {
+    await flutter_acrylic.Window.initialize();
+    await WindowManager.instance.ensureInitialized();
+    windowManager.waitUntilReadyToShow().then((_) async {
+      await windowManager.setTitleBarStyle(TitleBarStyle.hidden,
+          windowButtonVisibility: false);
+      await windowManager.setSize(const Size(755, 545));
+      await windowManager.setMinimumSize(const Size(755, 545));
+      await windowManager.center();
+      await windowManager.show();
+      await windowManager.setPreventClose(true);
+      await windowManager.setSkipTaskbar(false);
+    });
+  }
+  runApp(MyApp());
+}
+
+// void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Platform.isWindows
         ? fluent_ui.FluentApp(
-            home: Text('Hello!'),
+            title: 'Flutter App',
+            home: Text('Ejemplo de App con Flutter'),
           )
         : MaterialApp(
-            home: Text('Hello!'),
+            home: Scaffold(
+              appBar: AppBar(title: Text('Flutter App')),
+              body: Text('Ejemplo de App con Flutter'),
+            ),
           );
   }
 }
